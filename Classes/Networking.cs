62739c723b18e671989203572e47b21d2b362db5
miku-666 <74728189+NessieHax@users.noreply.github.com>
2022-04-04 22:27:57 +0200

    Simplified Networking.cs

--- Classes/Networking.cs
+++ Classes/Networking.cs
@@ -9,90 +9,51 @@ namespace FUI_Studio.Classes
     public static class Networking
     {
         public static string ver = "1.91";
-        static string url = "https://www.pckstudio.xyz/";
-        static string backupurl = "https://phoenixarc.github.io/pckstudio.tk/";
-        static string UpdateFilePath = "studio/FUI/api/update.txt";
+        static string url = "https://www.pckstudio.xyz";
+        static string backupurl = "https://phoenixarc.github.io/pckstudio.tk";
+        static string UpdateFilePath = "/studio/FUI/api/update.txt";
         public static bool NeedsUpdate = false;
-        static WebClient client = new WebClient();
-
-        public static void checkUpdate()
+        private static string Download(string apiPath, string errMsg = "")
         {
-            try
+            using (var client = new WebClient())
             {
-                if(float.Parse(client.DownloadString(url + UpdateFilePath)) > float.Parse(ver))
-                {
-                    NeedsUpdate = true;
-                    if (MessageBox.Show("Update " + client.DownloadString(url + UpdateFilePath) + " is available!\ndo you want to download it?\nYour current version is:" + ver, "Update available", MessageBoxButtons.YesNo) == DialogResult.Yes)
-                    {
-                        Process.Start(Environment.CurrentDirectory + "\\updater.exe");
-                        Application.Exit();
-                    }
-                }
-            }
-            catch
-            {
-                try
-                {
-                    if (float.Parse(client.DownloadString(backupurl + UpdateFilePath)) > float.Parse(ver))
-                    {
-                        NeedsUpdate = true;
-                        if (MessageBox.Show("Update " + client.DownloadString(backupurl + UpdateFilePath) + " is available!\ndo you want to download it?\nYour current version is:" + ver, "Update available", MessageBoxButtons.YesNo) == DialogResult.Yes)
-                        {
-                            Process.Start(Environment.CurrentDirectory + "\\updater.exe");
-                            Application.Exit();
-                        }
-                    }
-                }
+                string data = "";
+                try { data = client.DownloadString(url + UpdateFilePath); }
                 catch
                 {
-                    NeedsUpdate = false;
-                    MessageBox.Show("Server unreachable!\nUnable to check for updates!", "Server Error", MessageBoxButtons.OK);
+                    try { data = client.DownloadString(backupurl + UpdateFilePath); }
+                    catch { data = ""; }
                 }
+                finally
+                {
+                    if (string.IsNullOrEmpty(data))
+                    {
+                        throw new Exception($"Failed to get {apiPath}\n{errMsg}");
+                    }
+                }
+                return data;
             }
         }
 
-        public static void TryDlDatabse()
+        private static string getServerVersion()
         {
-            try
-            {
-                string WebDB = client.DownloadString(url + "studio/FUI/api/FUI_Image_Names.db");
-                string LocalDB = File.ReadAllText(Environment.CurrentDirectory + "\\Data\\FuiIMGData_Enc.db");
-                if (WebDB != LocalDB)
-                {
-                    DialogResult Dr = MessageBox.Show("Online Database is different from the one you have! do you want to proceed?", "Database Update available", MessageBoxButtons.YesNo);
-                    if (Dr == DialogResult.Yes)
-                    {
-                        File.WriteAllText(Environment.CurrentDirectory + "\\Data\\FuiIMGData_Enc.db", WebDB);
-                    }
-                    else if (Dr == DialogResult.No)
-                    {
+            return Download(UpdateFilePath);
+        }
 
-                    }
-                }
-            }
-            catch
-            {
-                try
-                {
-                    string WebDB = client.DownloadString(backupurl + "studio/FUI/api/FUI_Image_Names.db");
-                    string LocalDB = File.ReadAllText(Environment.CurrentDirectory + "\\Data\\FuiIMGData_Enc.db");
-                    if (WebDB != LocalDB)
-                    {
-                        DialogResult Dr = MessageBox.Show("Online Database is different from the one you have! do you want to proceed?", "Database Update available", MessageBoxButtons.YesNo);
-                        if (Dr == DialogResult.Yes)
-                        {
-                            File.WriteAllText(Environment.CurrentDirectory + "\\Data\\FuiIMGData_Enc.db", WebDB);
-                        }
-                        else if (Dr == DialogResult.No)
-                        {
+        public static void checkUpdate()
+        {
+            string server_version = getServerVersion();
+            NeedsUpdate = !server_version.Equals(ver);
+            if (NeedsUpdate) PromptForUpdate();
+        }
 
-                        }
-                    }
-                }
-                catch
-                {
-                    MessageBox.Show("Server Path unreachable!\nUnable to check Online Databse!", "Server Error", MessageBoxButtons.OK);
-                }
+        public static void PromptForUpdate()
+        {
+            string server_version = getServerVersion();
+            if (MessageBox.Show("Update " + server_version + " is available!\ndo you want to download it?\nYour current version is:" + ver, "Update available", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Process.Start(Environment.CurrentDirectory + "\\updater.exe");
+                Application.Exit();
             }
         }
     }
