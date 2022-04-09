fe259110da7b7f09b404829e260320f1a27c8b0a
miku-666 <74728189+NessieHax@users.noreply.github.com>
2022-04-09 18:16:42 +0200

    Add `TimelineEvent.ToArray()`

--- Classes/fui/TimelineEvent.cs
+++ Classes/fui/TimelineEvent.cs
@@ -49,8 +49,18 @@ namespace FUI_Studio.Classes.fui
         public byte[] ToArray()
         {
             var arr = new byte[GetByteSize()];
-            
 
+            arr[0] = EventType;
+            arr[1] = Unknown0;
+            arr[2] = (byte)ObjectType;
+            arr[3] = Unknown1;
+            BitConverter.GetBytes(Unknown2).CopyTo(arr, 4);
+            BitConverter.GetBytes(Index).CopyTo(arr, 6);
+            BitConverter.GetBytes(Unknown3).CopyTo(arr, 8);
+            BitConverter.GetBytes(NameIndex).CopyTo(arr, 10);
+            matrix.ToArray().CopyTo(arr, 12);
+            colorTransform.ToArray().CopyTo(arr, 0x24);
+            Color.ToArray().CopyTo(arr, 0x44);
             return arr;
         }
 
